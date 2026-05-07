a621c00b59937804fc48c1791e559325ca63273f
Kim <wdq123550@qq.com>
2026-05-07 13:56:50 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUIPlus",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftUIPlus",
+            targets: ["SwiftUIPlus"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftUIPlus"
+        ),
+        .testTarget(
+            name: "SwiftUIPlusTests",
+            dependencies: ["SwiftUIPlus"]
+        ),
+    ]
+)
